92bff94f76cb455df0afea87351259b7dfd7fba3
khushilad <120996371+mangoinatree@users.noreply.github.com>
2024-09-11 13:44:40 -0600

    About changes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import styles from './about.module.css'
-import { getImageUrl } from '../../utils'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChalkboardUser, faBook } from '@fortawesome/free-solid-svg-icons'
 
@@ -9,28 +8,28 @@ const About = () => {
   return (
     <section className={styles.container} id="about">
         <div className={styles.content}>
-            <img 
-                src={"/assets/about/aboutImage2.png"}
-                alt="Me sitting witha laptop"
-                className={styles.aboutImage}
-            />
+            
             <ul className={styles.aboutItems}>
                 <li className={styles.aboutItem}>
+                    <div className={styles.iconDiv}>
                     <FontAwesomeIcon icon={faChalkboardUser} className={styles.aboutItemIcon} size="3x"></FontAwesomeIcon>
+                    </div>
                     <div className={styles.aboutItemText}>
                     <h3>Programming Instuctor</h3>
                     <p>
-                        I have experience leading teams to teach 
-                        programming principles, javascript, C#, and web-developement.
+                        I lead teams to teach 
+                        programming principles, javascript, C#, web-developement, and 3D design.
                     </p>
                     </div>
                 </li>
                 <li className={styles.aboutItem}>
+                    <div className={styles.iconDiv}>
                     <FontAwesomeIcon icon={faBook} className={styles.aboutItemIcon} size="3x"></FontAwesomeIcon>
+                    </div>
                     <div className={styles.aboutItemText}>
                     <h3>Computing Science Student</h3>
                     <p>
-                        I am heading into my 3rd year of computing science! I gained valuable experience in data structure, databases, OOP, practical programming and more!
+                        I am in my 3rd year of computing science! I gained valuable experience in data structure, databases, OOP, software developement and more!
                     </p>
                     </div>
                 </li>
